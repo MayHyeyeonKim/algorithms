f519089b1a352a0323c5717d31188b9e859832e8
MayHyeyeonKim <hkim7963@gmail.com>
2023-03-20 12:47:44 -0700

    싱글리 링크드 리스트

---
+++ study/singly_linkedlist.py
@@ -0,0 +1,13 @@
+#Singly Linked List 
+
+class Node :
+    def __init__(self, value = 0, next = None):
+        self.value = value
+        self.next = next
+
+first = Node(1)
+second = Node(2)
+third = Node(3)
+first.next = second
+second.next = third
+first.value = 6
