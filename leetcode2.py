022f228089a92f86c5643bbddae4728a2b9f5c03
MayHyeyeonKim <hkim7963@gmail.com>
2023-03-19 23:55:28 -0700

    투썸 쉽다

--- leetcode2.py
+++ leetcode2.py
@@ -1,8 +1,12 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n= len(nums)
+def twoSum(nums, target):
+    n= len(nums)
 
-        for i in range(n):
-            for j in range(i+1, n):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
+    for i in range(n):
+        for j in range(i+1, n):
+            if nums[i] + nums[j] == target:
+                return [i,j]
+            
+nums = [2, 7, 11, 15]
+target = 9
+result = twoSum(nums, target)
+print(result)
