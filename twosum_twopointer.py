afd27ee39e06c957d2534d062d5c77f51e587bfa
MayHyeyeonKim <hkim7963@gmail.com>
2023-03-20 16:25:45 -0700

    twosum을 two pointer로 풀기

---
+++ twosum_twopointer.py
@@ -0,0 +1,15 @@
+def twosum(nums, target):
+    nums.sort
+    n = len(nums)
+    l,r = 0, n-1
+    while l<r:
+        if nums[l] + nums[r] > target:
+            r -= 1
+        elif nums[l] + nums[r] < target:
+            l += 1
+        elif nums[l] + nums[r] == target:
+            return True
+    return False
+
+result = twosum([1,2,3,4,5], 8)
+print(result)
