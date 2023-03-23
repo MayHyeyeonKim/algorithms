67eabdd0a2819f3288d2b09272a4c68b312217f1
MayHyeyeonKim <hkim7963@gmail.com>
2023-03-23 13:53:58 -0700

    윤년 파이썬

--- study/leapyear.py
+++ study/leapyear.py
@@ -1,11 +1,11 @@
-# year = int(input("Enter any year that is to be checked for leap year: "))
+year = int(input("Enter any year that is to be checked for leap year: "))
 
-# if year < 0:
-#     print("Invalid input: The year must be a positive integer")
-# elif (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-#     print(year, "is a leap year")
-# else:
-#     print(year, "is not a leap year")
+if year < 0:
+    print("Invalid input: The year must be a positive integer")
+elif (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+    print(year, "is a leap year")
+else:
+    print(year, "is not a leap year")
 
-i = 3
+
 
