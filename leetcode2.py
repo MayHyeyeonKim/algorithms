f396cef2b173eb859d6016d1c5c93e483531bc23
MayHyeyeonKim <hkim7963@gmail.com>
2023-03-28 01:30:14 -0700

    두 정수 리스트 까꾸로 된거 덧셈구현 핵어려움

--- leetcode2.py
+++ leetcode2.py
@@ -1,12 +1,33 @@
-def twoSum(nums, target):
-    n= len(nums)
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-    for i in range(n):
-        for j in range(i+1, n):
-            if nums[i] + nums[j] == target:
-                return [i,j]
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode(0)
+        curr = dummy
+        carry = 0
+        
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
             
-nums = [2, 7, 11, 15]
-target = 9
-result = twoSum(nums, target)
-print(result)
+            sum_ = val1 + val2 + carry
+            carry = sum_ // 10
+            curr.next = ListNode(sum_ % 10)
+            
+            curr = curr.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        
+        return dummy.next
+l1 = ListNode(2, ListNode(4, ListNode(3)))
+l2 = ListNode(5, ListNode(6, ListNode(4)))
+
+solution = Solution()
+result = solution.addTwoNumbers(l1, l2)
+
+while result:
+    print(result.val, end=" ")
+    result = result.next
