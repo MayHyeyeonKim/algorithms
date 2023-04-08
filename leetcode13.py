5d50e008ac33feb530acecce954edd5338747edc
MayHyeyeonKim <hkim7963@gmail.com>
2023-04-07 23:54:37 -0700

    로마자

--- leetcode13.py
+++ leetcode13.py
@@ -5,40 +5,35 @@ class Solution:
         prev = 0
         for c in s:
             current = roman_dict[c]
-            if prev == roman_dict['I'] and current == roman_dict['V']:
-                result += 3
-            elif prev == roman_dict['I'] and current == roman_dict['X']:
-                result += 8
-            elif prev == roman_dict['X'] and current == roman_dict['L']:
-                result += 30
-            elif prev == roman_dict['X'] and current == roman_dict['C']:
-                result += 80
-            elif prev == roman_dict['C'] and current == roman_dict['D']:
-                result += 300
-            elif prev == roman_dict['C'] and current == roman_dict['X']:
-                result += 800
-            elif current == roman_dict['I']:
-                result += 1
-            elif current == roman_dict['V']:
-                result += 5
-            elif current == roman_dict['X']:
-                result += 10
-            elif current == roman_dict['L']:
-                result += 50
-            elif current == roman_dict['C']:
-                result += 100
-            elif current == roman_dict['D']:
-                result += 500
-            elif current == roman_dict['M']:
-                result += 1000
+            if prev < current:
+                result += current - 2*prev
+            else:
+                result += current
+        #     if prev == roman_dict['I'] and current == roman_dict['V']:
+        #         result += 3
+        #     elif prev == roman_dict['I'] and current == roman_dict['X']:
+        #         result += 8
+        #     elif prev == roman_dict['X'] and current == roman_dict['L']:
+        #         result += 30
+        #     elif prev == roman_dict['X'] and current == roman_dict['C']:
+        #         result += 80
+        #     elif prev == roman_dict['C'] and current == roman_dict['D']:
+        #         result += 300
+        #     elif prev == roman_dict['C'] and current == roman_dict['X']:
+        #         result += 800
+        #     elif current == roman_dict['I']:
+        #         result += 1
+        #     elif current == roman_dict['V']:
+        #         result += 5
+        #     elif current == roman_dict['X']:
+        #         result += 10
+        #     elif current == roman_dict['L']:
+        #         result += 50
+        #     elif current == roman_dict['C']:
+        #         result += 100
+        #     elif current == roman_dict['D']:
+        #         result += 500
+        #     elif current == roman_dict['M']:
+        #         result += 1000
             prev = current
         return result
-
-
-#TestCase3 -> Wrong Answer
-# Input
-# s = "MCMXCIV"
-# Output
-# 2194
-# Expected
-# 1994
