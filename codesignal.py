68972d8e8922302bfb9adb619628272f4d03510f
MayHyeyeonKim <hkim7963@gmail.com>
2023-04-09 00:59:37 -0700

    몰라 기억나는대로

---
+++ codesignal.py
@@ -0,0 +1,14 @@
+diffs = [-1000,-500]
+
+res = []
+init = 1500
+if len(diffs) > 0:
+    for num in diffs:
+        init += num
+        res.append(init)
+    my_list = [max(res), max(res)-abs(res[-1])]
+    if len(diffs) == 2: 
+        my_list = [max(res), min(res)]
+else:
+    my_list = [init, init]
+print(my_list)
