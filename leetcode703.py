dac5f82a45cc91c3bd49d2dcb396a139771d18eb
MayHyeyeonKim <hkim7963@gmail.com>
2023-04-26 23:51:56 -0700

    이거는 easy가 아니라 medium이다..

---
+++ leetcode703.py
@@ -0,0 +1,36 @@
+import heapq
+
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.min_heap = []
+        for num in nums:
+            self.add(num)
+    
+    def add(self, val: int) -> int:
+        heapq.heappush(self.min_heap, val)
+        if len(self.min_heap) > self.k:
+            heapq.heappop(self.min_heap)
+        return self.min_heap[0]
+
+
+class KthLargest1:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.minHeap, self.k = nums,k
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
+
+
