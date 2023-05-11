5d6a394def04097dea4e5cf980d35b3d6df3fe71
MayHyeyeonKim <hkim7963@gmail.com>
2023-05-11 00:14:31 -0700

    큐 사용해서 3초동안 발생한 요청수 반환

---
+++ leetcode933_1.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+class RecentCounter:
+
+    def __init__(self):
+        self.queue = deque()
+
+    def ping(self, t):
+        q = self.queue
+        self.queue.append(t)
+        start = t - 3000
+        while q and q[0] < start:
+            q.popleft()
+        return len(q)
+    
+test = RecentCounter()
+print(test.ping(1))
+print(test.ping(100))
+print(test.ping(3001))
+print(test.ping(3003))
