7b14b14b1301a9fb44691a8ee1d2c9ecb0708f34
hyeyeonkim <hyeyeonkim@hyeyeonkimui-MacBookPro.local>
2023-06-02 04:57:03 -0700

    보석, 해시테이블

---
+++ leetcode771.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        freqs = {}
+
+        for char in stones:
+            if char not in freqs:
+                freqs[char] = 1
+            else:
+                freqs[char] += 1
+
+        count = 0
+        for char in jewels:
+            if char in freqs:
+                count += freqs[char]
+
+        return count
