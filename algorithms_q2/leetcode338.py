0d4714d8bd1e3761973994042350292205ba9da8
hyeyeonkim <hkim7963@gmail.com>
2023-07-10 02:16:53 -0700

    DP

---
+++ algorithms_q2/leetcode338.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        bits = [0] * (n + 1)  # Initialize the bits array with zeros
+
+        for i in range(1, n + 1):
+            bits[i] = bits[i >> 1] + (i & 1)
+
+        return bits
