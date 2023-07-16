ea60d60829eb6f43f76bb5fd8157e7851b5ad723
hyeyeonkim <hkim7963@gmail.com>
2023-07-16 11:00:00 +0000

    leetcode1337

--- algorithms_q2/leetcode1337.py
+++ algorithms_q2/leetcode1337.py
@@ -2,18 +2,25 @@ class Solution:
     import heapq
 
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        lis = []
-        for i in range(len(mat)):
-            count = 0
-            for j in range(len(mat[0])):
-                if mat[i][j] == 0:
-                    break
-                else:
-                    count += 1
-            heapq.heappush(lis, [count, i])
-        final = []
-        while k:
-            a = heapq.heappop(lis)
-            final.append(a[1])
-            k -= 1
-        return final
+        # lis = []
+        # for i in range(len(mat)):
+        #     count = 0
+        #     for j in range(len(mat[0])):
+        #         if mat[i][j] == 0:
+        #             break
+        #         else:
+        #             count += 1
+        #     heapq.heappush(lis, [count, i])
+        # final = []
+        # while k:
+        #     a = heapq.heappop(lis)
+        #     final.append(a[1])
+        #     k -= 1
+        # return final
+
+        tmp = []
+        for i, m in enumerate(mat):
+            cand = (sum(m), i)
+            tmp.append(cand)
+        tmp.sort()
+        return [i[1] for i in tmp[:k]]
