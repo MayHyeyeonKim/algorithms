d0de3dde7328eef06b40332c33ba7c2f53934c77
hyeyeonkim <hkim7963@gmail.com>
2023-08-26 23:25:14 -0700

    DP: min cost climbing strairs

---
+++ algorithms_q2/leetcode746.py
@@ -0,0 +1,20 @@
+# 완전탐색
+
+# cost = [10, 15, 20, 17, 1]
+# def dfs(n): #O(2^n)
+#     if n == 0 and n == 1:
+#         return 0
+#     return min(dfs(n - 1) + cost[n - 1], dfs(n - 2) + cost[n - 2])
+
+# top down(memoization)
+
+cost = [10, 15, 20, 17, 1]
+memo = {}
+
+
+def dp(n):  # O(n)
+    if n == 0 and n == 1:
+        return 0
+    if n not in memo:
+        min(dp(n - 1) + cost[n - 1], dp(n - 2) + cost[n - 2])
+    return memo[n]
