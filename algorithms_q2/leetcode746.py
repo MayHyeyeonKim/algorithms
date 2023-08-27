413736c3ea6b19ea5b8c5e4428078ffee58f2fbf
hyeyeonkim <hkim7963@gmail.com>
2023-08-26 23:34:29 -0700

    DP: min cost climbing strairs

--- algorithms_q2/leetcode746.py
+++ algorithms_q2/leetcode746.py
@@ -1,3 +1,18 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        memo = {}
+
+        def dp(n):
+            if n == 0 or n == 1:
+                return 0
+            if n not in memo:
+                memo[n] = min(dp(n - 1) + cost[n - 1], dp(n - 2) + cost[n - 2])
+            return memo[n]
+
+        return dp(n)
+
+
 # 완전탐색
 
 # cost = [10, 15, 20, 17, 1]
@@ -8,13 +23,13 @@
 
 # top down(memoization)
 
-cost = [10, 15, 20, 17, 1]
-memo = {}
+# cost = [10, 15, 20, 17, 1]
+# memo = {}
 
 
-def dp(n):  # O(n)
-    if n == 0 and n == 1:
-        return 0
-    if n not in memo:
-        min(dp(n - 1) + cost[n - 1], dp(n - 2) + cost[n - 2])
-    return memo[n]
+# def dp(n):  # O(n)
+#     if n == 0 and n == 1:
+#         return 0
+#     if n not in memo:
+#         min(dp(n - 1) + cost[n - 1], dp(n - 2) + cost[n - 2])
+#     return memo[n]
