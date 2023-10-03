2c6e51730a645016dd6f384e6680fb068cba3200
hyeyeonkim <hkim7963@gmail.com>
2023-10-03 15:30:00 -0700

    leetcode100

---
+++ DFSBFS30/leetcode100.py
@@ -0,0 +1,30 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode):
+        if not q and not p:
+            return True
+        if not q or not p:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+
+# 테스트를 위해 두 이진 트리를 생성
+tree1 = TreeNode(1)
+tree1.left = TreeNode(2)
+tree1.right = TreeNode(3)
+
+tree2 = TreeNode(1)
+tree2.left = TreeNode(2)
+tree2.right = TreeNode(3)
+
+sol = Solution()
+result = sol.isSameTree(tree1, tree2)
+print(result)
