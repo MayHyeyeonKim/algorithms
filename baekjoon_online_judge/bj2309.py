1a4c06d3f52d2a2840ede66b35d0291a4fde0e91
hyeyeonkim <hkim7963@gmail.com>
2023-10-23 19:31:33 -0700

    rm baekjoon

--- baekjoon_online_judge/bj2309.py
+++
@@ -1,8 +0,0 @@
-from itertools import combinations
-
-height = [int(input()) for _ in range(9)]
-for combi in combinations(height, 7):
-    if sum(combi) == 100:
-        for height in sorted(combi):
-            print(height, end=" ")
-        break
