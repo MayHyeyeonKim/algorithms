ecb78a7db8152a9d958aca6b222c583c7292878d
MayHyeyeonKim <hkim7963@gmail.com>
2024-01-26 11:00:00 +0000

    Longest Substring Without Repeating Characters

---
+++ AppleSpring23Frequency/LC3_LongestSubstringWithRepeatingCharacters.js
@@ -0,0 +1,16 @@
+function lengthOfLongestSubstring(s) {
+  let set = new Set();
+  let left = 0;
+  let maxLength = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    while (set.has(s[right])) {
+      set.delete(s[left]);
+      left++;
+    }
+    set.add(s[right]);
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
+}
