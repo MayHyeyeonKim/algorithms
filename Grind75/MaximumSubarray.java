5c6970341c767185ec6b06af2803b0de9cdd209e
MayHyeyeonKim <hkim7963@gmail.com>
2024-01-28 00:19:20 -0800

    LC53 Maximum Subarray

---
+++ Grind75/MaximumSubarray.java
@@ -0,0 +1,26 @@
+package Grind75;
+
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        int curSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            curSum = Math.max(num, curSum + num);
+            maxSum = Math.max(curSum, maxSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        MaximumSubarray solution = new MaximumSubarray();
+    
+            // Example input
+            int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    
+            // Call maxSubArray method and print the result
+            int result = solution.maxSubArray(nums);
+            System.out.println("Maximum Subarray Sum: " + result);
+    }
+}
