a621343115d2d26bd3bfb2bc6b797123fdebc452
MayHyeyeonKim <hkim7963@gmail.com>
2024-03-19 23:41:21 -0700

    created example lists for LC21 Merge Two Sorted Lists

--- neetCode150/py/21MergeTwoSortedLists.py
+++ neetCode150/py/21MergeTwoSortedLists.py
@@ -1,6 +1,11 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        cur = dummy = ListNode()
+    def mergeTwoLists(self, list1, list2):
+        cur = dummy = ListNode(0)
         while list1 and list2:
             if list1.val <= list2.val:
                 cur.next = list1
@@ -11,3 +16,22 @@ class Solution:
         if list1 or list2:
             cur.next = list1 if list1 else list2
         return dummy.next
+
+def printList(head):
+    while head:
+        print(head.val, end=" -> ")
+        head = head.next
+    print("None")
+
+# Example lists
+list1 = ListNode(1, ListNode(2, ListNode(4)))
+list2 = ListNode(1, ListNode(3, ListNode(5)))
+
+# Create a Solution object
+solution = Solution()
+
+# Merge the two lists
+merged_list = solution.mergeTwoLists(list1, list2)
+
+# Print the merged list
+printList(merged_list)
--- neetCode150/py/287FindtheDuplicateNumber.py
+++ neetCode150/py/287FindtheDuplicateNumber.py
@@ -1,7 +1,24 @@
 class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        visited = set()
-        for ele in nums:
-            if ele in visited:
-                return ele
-            visited.add(ele)
+    def findDuplicate(self, nums):
+        # Find cycle
+        slow = nums[0]
+        fast = nums[0]
+
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+        # Find starting point of cycle
+        slow = nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return slow
+
+nums = [2,9,7,6,2]
+sol = Solution()
+result = sol.findDuplicate(nums)
+print(result)
