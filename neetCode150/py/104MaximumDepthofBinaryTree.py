eaa8dafff869ce73ffba5d34911998d4405efecb
MayHyeyeonKim <hkim7963@gmail.com>
2024-03-22 02:11:49 -0700

    maximum Depth of Binary Tree

--- neetCode150/py/104MaximumDepthofBinaryTree.py
+++ neetCode150/py/104MaximumDepthofBinaryTree.py
@@ -8,7 +8,6 @@ class Solution:
     def maxDepth(self, root):
         if not root:
             return 0
-        # [bool, depth]
         left = self.maxDepth(root.left)
         right = self.maxDepth(root.right)
         return 1 + max(left, right)
