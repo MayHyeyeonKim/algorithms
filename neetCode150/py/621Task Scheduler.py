675c99cf14f54fa23d01f1b7048c9145ef0a8a89
MayHyeyeonKim <hkim7963@gmail.com>
2024-04-20 23:43:52 -0700

    heap

--- neetCode150/py/621Task Scheduler.py
+++ neetCode150/py/621Task Scheduler.py
@@ -1,1 +1,28 @@
 # 621Task Scheduler.py
+# 26char
+import heapq
+from collections import deque
+from typing import Counter
+
+class Solution:
+    def leastInterval(self, tasks, n):
+        count = Counter(tasks)
+        maxHeap = [ -cnt for cnt in count.values()]
+        heapq.heapify(maxHeap)
+
+        time = 0
+        q = deque()
+        while maxHeap or q:
+            time += 1
+            if maxHeap:
+                cnt = 1 + heapq.heappop(maxHeap)
+                if cnt:
+                    q.append([cnt, time+n])
+            if q and q[0][1] == time:
+                heapq.heappush(maxHeap, q.popleft()[0])
+        return time
+
+
+sol = Solution()
+result = sol.leastInterval(tasks=["A","A","B","C","C","D"], n=4)
+print(result)
