e3fdb1b541ce2d89fcdf08594ca77e384358c7db
MayHyeyeonKim <hkim7963@gmail.com>
2024-05-18 16:55:47 -0700

    backtracking

---
+++ neetCode150/py/131PalindromePartitioning.py
@@ -0,0 +1,28 @@
+class Solution:
+    def partition(self, s):
+        res = []
+        part = []
+        
+        def dfs(i):
+            if i >= len(s):
+                res.append(part.copy())
+                return
+            for j in range(i, len(s)):
+                if self.isPali(s, i, j):
+                    part.append(s[i:j+1])
+                    dfs(j + 1)
+                    part.pop()
+
+        dfs(0)
+        return res
+
+    def isPali(self, s: str, l: int, r: int) -> bool:
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l, r = l + 1, r - 1
+        return True
+
+sol = Solution()
+result = sol.partition("aad")
+print(result)
