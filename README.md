a8b68b5f66b0a330bc8f7913a929f9f1e6994f8c
MayHyeyeonKim <hkim7963@gmail.com>
2024-07-06 12:25:55 -0700

    dp

--- README.md
+++ README.md
@@ -68,7 +68,7 @@
 - [Longest Common Subsequence](https://leetcode.com/problems/longest-common-subsequence/) - Medium - 20240701
 - [Best Time to Buy and Sell Stock with Cooldown](https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/) - Medium - 20240702
 - [Coin Change II](https://leetcode.com/problems/coin-change-ii/) - Medium - 20240703
-- [Target Sum](https://leetcode.com/problems/target-sum/) - Medium
+- [Target Sum](https://leetcode.com/problems/target-sum/) - Medium - 20240705
 - [Interleaving String](https://leetcode.com/problems/interleaving-string/) - Medium
 - [Longest Increasing Path in a Matrix](https://leetcode.com/problems/longest-increasing-path-in-a-matrix/) - Hard
 - [Distinct Subsequences](https://leetcode.com/problems/distinct-subsequences/) - Hard
---
+++ neetCode150/py/TargetSum.py
@@ -0,0 +1,70 @@
+# nums의 요소에 + 또는 - 기호를 붙여서 target을 만들 수 있는 방법의 수 반환
+# findTSW(Target Sum Ways)
+
+# class Solution:
+#     def findTargetSumWays(self, nums, target):
+#         count = 0
+#         dp = {}
+#         n = len(nums)
+        
+#         def findTSW(i, sum):
+#             if (i, sum) in dp:
+#                 return dp[(i, sum)]
+#             if (i == n):
+#                 if (sum == target):
+#                     return 1
+#                 return 0
+#             addElem = findTSW(i + 1, sum + nums[i])
+#             remElem = findTSW(i + 1, sum - nums[i])
+#             dp[(i, sum)] = addElem + remElem
+#             return addElem + remElem
+        
+#         return findTSW(0, 0)
+
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        can = collections.defaultdict(int)
+        can[0] = 1
+
+        # can[0] = 1
+
+        # can[-1] = 1
+        # can[1] = 1
+        # can.keys() = -1, 1
+
+        # -1 -> -2, 0
+        # +1 -> 0, 2
+        # can[-2] = 1
+        # can[0] = 2
+        # can[2] = 1
+        # can.keys() = -2, 0, 2
+
+        # -1 -> -3, -1, 1
+        # +1 -> -1, 1, 3
+        # can[-3] = can[-2] = 1
+        # can[-1] = 2 + 1 = 3
+        # can[1] = can[2] + can[0] = 1 + 2 = 3
+        # can[3] = can[2] = 1
+        # can.keys() = -3, -1, 1, 3
+
+        # -1 -> -4, -2, 0, 2
+        # +1 -> -2, 0, 2, 4
+        # can[-4] = can[-3] = 1
+        # can[-2] = can[-1] + can[-3] = 3 + 1 = 4
+        # can[0] = can[1] + can[-1] = 3 + 3 = 6
+        # can[2] = can[3] + can[1] = 1 + 3 = 4
+        # can[4] = can[3] = 1
+        # can.keys() = -4,-2,0,2,4
+
+        # -1 -> -5,-3,-1,1,3
+        # +1 -> -3,-1,1,3,5
+        # can[3] = can[4] + can[2] = 1 + 4 = 5
+        for n in nums:
+            new_can = collections.defaultdict(int)
+            for p in can.keys():
+                new_can[p-n] += can[p]
+                new_can[p+n] += can[p]
+            can = new_can
+            print(can)
+
+        return can[target]
