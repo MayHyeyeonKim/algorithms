0f55ad60d73f0bb1da872c58ffb8183cb71b33d9
MayHyeyeonKim <hkim7963@gmail.com>
2024-07-18 00:16:27 -0700

    543. Diameter of Binary Tree

--- study/PY/Trees/BFS_DiameterofBinaryTree.py
+++ study/PY/Trees/BFS_DiameterofBinaryTree.py
@@ -23,10 +23,27 @@ class Solution:
     
 root = TreeNode(1)
 root.left = TreeNode(2)
-root.right = TreeNode(3)
-root.left.left = TreeNode(4)
-root.right.right = TreeNode(5)
+root.left.left = TreeNode(3)
+root.left.right = TreeNode(4)
+root.left.left.left = TreeNode(5)
 
 sol = Solution()
 result = sol.diameterOfBinaryTree(root)
 print(result)
+
+# 트리의 지름은 가장 긴 경로의 길이
+
+# res = max(res, height(currentNode.left) + height(currentNode.right))
+# 각 노드를 중심으로 왼쪽 서브트리와 오른쪽 서브트리의 높이를 더한 값이 그 노드를 루트로 하는 경로의 길이
+# height(currentNode.left) + height(currentNode.right)는 현재 노드를 루트로 하는 경로의 길이를 계산
+
+
+# if currentNode.left:
+#     queue.append(currentNode.left)
+# if currentNode.right:
+#     queue.append(currentNode.right)
+# 가장 긴 경로가 트리의 어느 위치에 있을지 알 수 없기 때문에 모든 노드를 다 탐색해 봐야 해서 queue에 모든 노드를 담아 모든 노드의 res를 구해본다. 
+
+# 챗지피티 왈
+#  이진 트리의 지름(가장 긴 경로)은 트리의 어느 위치에 있을지 알 수 없기 때문에 모든 노드를 탐색하여 각 노드를 루트로 하는 경로의 길이를 계산해야 합니다. 
+# 이를 위해 큐를 사용하여 너비 우선 탐색(BFS) 방식으로 모든 노드를 방문하고 각 노드에서 가능한 최대 경로의 길이를 계산하는 것입니다.
