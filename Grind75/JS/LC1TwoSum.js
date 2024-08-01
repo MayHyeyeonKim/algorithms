4afb5df6b5dcefd76497e81cc4895af0098e076b
MayHyeyeonKim <hkim7963@gmail.com>
2024-07-31 23:52:11 -0700

    twosum

--- Grind75/JS/LC1TwoSum.js
+++ Grind75/JS/LC1TwoSum.js
@@ -1,18 +1,18 @@
-// Approach 1: Brute Force
-function twoSum1(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
-        }
-    }
-    return [];
-}
+// // Approach 1: Brute Force
+// function twoSum1(nums, target) {
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if (nums[i] + nums[j] === target) {
+//                 return [i, j];
+//             }
+//         }
+//     }
+//     return [];
+// }
 
-const nums1 = [2, 7, 11, 15];
-const target1 = 9;
-console.log(twoSum1(nums1, target1));
+// const nums1 = [2, 7, 11, 15];
+// const target1 = 9;
+// console.log(twoSum1(nums1, target1));
 
 // Approach 2: Using a Hash Map
 function twoSum2(nums, target) {
@@ -31,27 +31,32 @@ const nums2 = [2, 7, 11, 15];
 const target2 = 9;
 console.log(twoSum2(nums2, target2));
 
-// Approach 3: Two Pointers (For sorted arrays)
-function twoSumTwoPointers(nums, target) {
-    const indexedNums = nums.map((num, index) => [num, index]);
-    indexedNums.sort((a, b) => a[0] - b[0]);
 
-    let left = 0;
-    let right = indexedNums.length - 1;
+// map.set('a', 1);는 키 'a'에 값 1을 설정하는 것
+// map.get('a')는 키 'a'에 해당하는 값을 반환하는 것
+// map.has('a'); // true: 'a' 키가 존재함 (bool값)
 
-    while (left < right) {
-        const sum = indexedNums[left][0] + indexedNums[right][0];
-        if (sum === target) {
-            return [indexedNums[left][1], indexedNums[right][1]];
-        } else if (sum < target) {
-            left++;
-        } else {
-            right--;
-        }
-    }
-    return [];
-}
+// // Approach 3: Two Pointers (For sorted arrays)
+// function twoSumTwoPointers(nums, target) {
+//     const indexedNums = nums.map((num, index) => [num, index]);
+//     indexedNums.sort((a, b) => a[0] - b[0]);
 
-const nums3 = [2, 7, 11, 15];
-const target3 = 9;
-console.log(twoSumTwoPointers(nums3, target3));
+//     let left = 0;
+//     let right = indexedNums.length - 1;
+
+//     while (left < right) {
+//         const sum = indexedNums[left][0] + indexedNums[right][0];
+//         if (sum === target) {
+//             return [indexedNums[left][1], indexedNums[right][1]];
+//         } else if (sum < target) {
+//             left++;
+//         } else {
+//             right--;
+//         }
+//     }
+//     return [];
+// }
+
+// const nums3 = [2, 7, 11, 15];
+// const target3 = 9;
+// console.log(twoSumTwoPointers(nums3, target3));
