6d08cd973585565bfc8c3b76084ee867b162f549
MayHyeyeonKim <hkim7963@gmail.com>
2024-08-10 21:56:19 -0700

    prepare co-te

--- Grind75/JS/LC121BestTimetoBuyandSellStock.js
+++ Grind75/JS/LC121BestTimetoBuyandSellStock.js
@@ -10,9 +10,11 @@ function maxProfit(prices) {
         }
     }
 
-    return maxProfit;
+    return maxProfit; // 함수명과 변수명이 같으면 그 변수는 함수 내부에서 우선적으로 사용됨. 변수 섀도잉
 }
 
 // Example usage:
 const prices = [7, 1, 5, 3, 6, 4];
 console.log(maxProfit(prices)); // Output: 5
+
+// JavaScript는 변수와 함수 이름이 충돌하지 않도록 로컬 스코프와 전역 스코프를 분리하여 관리하기에 함수 이름과 함수 내부의 로컬 변수 이름이 같더라도 문제가 발생하지 않으며, 로컬 변수가 우선적으로 사용됨.
--- Grind75/JS/LC20ValidParentheses.js
+++ Grind75/JS/LC20ValidParentheses.js
@@ -25,3 +25,34 @@ console.log(isValid("()[]{}")); // true
 console.log(isValid("(]")); // false
 console.log(isValid("([)]")); // false
 console.log(isValid("{[]}")); // true
+
+
+//카운터 어프로치
+// var isValid = function(s) {
+//     let parenthesesCount = 0;
+//     let curlyBracesCount = 0;
+//     let squareBracketsCount = 0;
+
+//     for (let char of s) {
+//         if (char === '(') {
+//             parenthesesCount++;
+//         } else if (char === ')') {
+//             parenthesesCount--;
+//             if (parenthesesCount < 0) return false;
+//         } else if (char === '{') {
+//             curlyBracesCount++;
+//         } else if (char === '}') {
+//             curlyBracesCount--;
+//             if (curlyBracesCount < 0) return false;
+//         } else if (char === '[') {
+//             squareBracketsCount++;
+//         } else if (char === ']') {
+//             squareBracketsCount--;
+//             if (squareBracketsCount < 0) return false;
+//         }
+//     }
+
+//     return parenthesesCount === 0 && curlyBracesCount === 0 && squareBracketsCount === 0;
+// };
+
+//([)]"와 같은 경우, 열림 괄호 (와 닫힘 괄호 )의 개수는 맞지만, 순서가 맞지 않기 때문에 이 코드는 잘못된 결과를 반환
