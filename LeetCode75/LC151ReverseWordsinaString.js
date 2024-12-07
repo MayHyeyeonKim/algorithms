5497c70e35cab5869ec5adebffd6e9f038f11167
MayHyeyeonKim <hkim7963@gmail.com>
2024-12-06 22:06:35 -0800

    reverse words in a string

--- LeetCode75/LC151ReverseWordsinaString.js
+++ LeetCode75/LC151ReverseWordsinaString.js
@@ -1,1 +1,17 @@
 // 151. Reverse Words in a String
+
+var reverseWords = function (s) {
+  let words = [];
+  let i = 0;
+
+  while (s[i] === " " && i < s.length) i++;
+  while (i < s.length) {
+    let wordStart = i;
+    while (s[i] !== " " && i < s.length) i++;
+    let wordEnd = i;
+    words.push(s.slice(wordStart, wordEnd));
+    while (s[i] === " " && i < s.length) i++;
+  }
+
+  return words.reverse().join(" ");
+};
