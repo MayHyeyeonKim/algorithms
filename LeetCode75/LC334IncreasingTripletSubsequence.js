8cd75a2fe471eb49b38038c9086524149c76f56c
MayHyeyeonKim <hkim7963@gmail.com>
2024-12-23 00:15:08 -0800

    LC334 Increasing Triple Subsequence

--- LeetCode75/LC334IncreasingTripletSubsequence.js
+++ LeetCode75/LC334IncreasingTripletSubsequence.js
@@ -1,16 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
 var increasingTriplet = function (nums) {
   let first = Infinity;
   let second = Infinity;
 
   for (let num of nums) {
     if (num <= first) {
-      first = num; // 첫 번째로 작은 숫자를 업데이트
+      first = num;
     } else if (num <= second) {
-      second = num; // 두 번째로 작은 숫자를 업데이트
+      second = num;
     } else {
-      return true; // 세 번째로 큰 숫자를 찾으면 true 반환
+      return true;
     }
   }
-
-  return false; // 트리플릿을 찾지 못하면 false 반환
+  return false;
 };
