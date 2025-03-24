6f85f73791855ba5bd369387978a71e16740ed34
MayHyeyeonKim <hkim7963@gmail.com>
2025-03-23 21:17:38 -0700

    two-pointer

--- TikTokSpring23HF/209_MinimumSizeSubarraySum.js
+++ TikTokSpring23HF/209_MinimumSizeSubarraySum.js
@@ -1,1 +1,18 @@
 // 209. Minimum Size Subarray Sum
+function minSubArrayLen(target, nums) {
+  let left = 0;
+  let sum = 0;
+  let minLen = Infinity;
+
+  for (let right = 0; right < nums.length; right++) {
+    sum += nums[right];
+
+    while (sum >= target) {
+      minLen = Math.min(minLen, right - left + 1);
+      sum -= nums[left];
+      left++;
+    }
+  }
+
+  return minLen === Infinity ? 0 : minLen;
+}
