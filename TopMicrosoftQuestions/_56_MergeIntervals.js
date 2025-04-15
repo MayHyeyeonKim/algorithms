5b0db1647bbd6a13e81169c8d487b75f327b2b52
MayHyeyeonKim <hkim7963@gmail.com>
2025-04-14 20:14:28 -0700

    merged intervals

--- TopMicrosoftQuestions/_56_MergeIntervals.js
+++ TopMicrosoftQuestions/_56_MergeIntervals.js
@@ -1,1 +1,18 @@
 // 56. Merge Intervals
+var merge = function (intervals) {
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  const result = [intervals[0]];
+
+  for (i = 1; i < intervals.length; i++) {
+    const [prevStart, prevEnd] = result[result.length - 1];
+    const [curStart, curEnd] = intervals[i];
+
+    if (curStart <= prevEnd) {
+      result[result.length - 1][1] = Math.max(prevEnd, curEnd);
+    } else {
+      result.push([curStart, curEnd]);
+    }
+  }
+  return result;
+};
