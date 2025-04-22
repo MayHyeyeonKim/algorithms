ff781f3620285a4fccebc2c71adf8a67339b1169
MayHyeyeonKim <hkim7963@gmail.com>
2025-04-22 01:30:07 -0700

    3sum

---
+++ TopMicrosoftQuestions/ThreeSum.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class ThreeSum {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int target = -nums[i];
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                if (sum == target) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    while (left < right && nums[left] == nums[left + 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right - 1])
+                        right--;
+
+                    left++;
+                    right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ThreeSum ts = new ThreeSum();
+        int[] nums = { -1, 0, 1, 2, -1, -4 };
+        List<List<Integer>> res = ts.threeSum(nums);
+        System.out.println(res);
+    }
+}
