50c1fe63969eb934ea877bf623430391598a68bc
MayHyeyeonKim <hkim7963@gmail.com>
2025-04-30 12:55:57 -0700

    set config

---
+++ jest.config.mjs
@@ -0,0 +1,14 @@
+// jest.config.mjs
+export default {
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.ts$": ["ts-jest", { useESM: true }],
+  },
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+};
+
+// npx ts -jest config:init 한번 해줘야 이게 생성됨
---
+++ src/toBe.spec.ts
@@ -0,0 +1,5 @@
+import sum from "./toBe"
+
+test('The sum function should add two nums', () => {
+    expect(sum(2, 4)).toBe(6)
+})
