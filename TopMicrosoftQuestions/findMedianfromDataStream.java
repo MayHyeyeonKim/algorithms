3c74d464987efdee29681eb75a949d0d50045b84
MayHyeyeonKim <hkim7963@gmail.com>
2025-04-29 18:49:22 -0700

    훗 이쯤이야

--- TopMicrosoftQuestions/findMedianfromDataStream.java
+++ TopMicrosoftQuestions/findMedianfromDataStream.java
@@ -1,5 +1,44 @@
 // 295. Find Median from Data Stream
 
-public class findMedianfromDataStream {
+import java.util.Collections;
+import java.util.PriorityQueue;
 
+public class findMedianfromDataStream {
+    private PriorityQueue<Integer> maxHeap;
+    private PriorityQueue<Integer> minHeap;
+
+    public findMedianfromDataStream() {
+        maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        minHeap = new PriorityQueue<>();
+    }
+
+    public void addNum(int num) {
+        // 먼저 maxH에 넣어주고
+        maxHeap.offer(num);
+        // maxH의 최대값이 minH의 최소값보다 크면 이동
+        minHeap.offer(maxHeap.poll());
+        // maxHeap이 더 많아야 하니까 다시 rebalance
+        if (maxHeap.size() < minHeap.size()) {
+            maxHeap.offer(minHeap.poll());
+        }
+    }
+
+    public double findMedian() { // 더블은 소수점 표현가능 숫자타입
+        if (maxHeap.size() == minHeap.size()) {
+            return (maxHeap.peek() + minHeap.peek()) / 2.0;
+        } else {
+            return maxHeap.peek();
+        }
+    }
+
+    public static void main(String[] args) {
+        findMedianfromDataStream mf = new findMedianfromDataStream();
+
+        mf.addNum(1);
+        mf.addNum(2);
+        System.out.println("Median: " + mf.findMedian());
+
+        mf.addNum(3);
+        System.out.println("Median: " + mf.findMedian());
+    }
 }
