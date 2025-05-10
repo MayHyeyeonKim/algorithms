464312b62d2d7a2a42357320d88178e96bbf1c4d
MayHyeyeonKim <hkim7963@gmail.com>
2025-05-09 21:47:10 -0700

    Learned how to compute diagonal groups

--- TopMicrosoftQuestions/_498_DiagonalTraverse.js
+++ TopMicrosoftQuestions/_498_DiagonalTraverse.js
@@ -1,1 +1,30 @@
 // 498. Diagonal Traverse
+function findDiagonalOrder(matrix) {
+  const map = new Map();
+  const ROWS = matrix.length;
+  const COLS = matrix[0].length;
+
+  for (let r = 0; r < ROWS; r++) {
+    for (let c = 0; c < COLS; c++) {
+      const key = r + c;
+      if (!map.has(key)) map.set(key, []);
+      map.get(key).push(matrix[r][c]);
+    }
+  }
+  const res = [];
+  const maxKey = ROWS + COLS - 1;
+  for (let i = 0; i < maxKey; i++) {
+    if (map.has(i)) {
+      const group = i % 2 === 0 ? map.get(i).reverse() : map.get(i);
+      res.push(...group);
+    }
+  }
+  return res;
+}
+
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+console.log(findDiagonalOrder(matrix));
