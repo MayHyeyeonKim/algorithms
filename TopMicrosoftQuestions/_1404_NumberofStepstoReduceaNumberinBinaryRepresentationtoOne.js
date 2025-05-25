ff91015179dc8b1f96263be7edeeebcd6642e9dc
MayHyeyeonKim <hkim7963@gmail.com>
2025-05-24 23:24:04 -0700

    used BigInt

--- TopMicrosoftQuestions/_1404_NumberofStepstoReduceaNumberinBinaryRepresentationtoOne.js
+++ TopMicrosoftQuestions/_1404_NumberofStepstoReduceaNumberinBinaryRepresentationtoOne.js
@@ -5,3 +5,21 @@
     (2^3)(2^2)(2^1)(2^0)
       8 + 4 + 0 + 1 = 13
 */
+
+function numSteps(s) {
+  let num = BigInt("0b" + s);
+  let steps = 0;
+
+  while (num > 1n) {
+    if (num % 2n === 0n) {
+      num /= 2n;
+    } else {
+      num += 1n;
+    }
+    steps++;
+  }
+  return steps;
+}
+
+let num = "1101";
+console.log(numSteps(num));
