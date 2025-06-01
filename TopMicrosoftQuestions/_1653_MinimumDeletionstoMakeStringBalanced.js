bc05f54120684f638ef2fde57b120b496fbc8783
MayHyeyeonKim <hkim7963@gmail.com>
2025-05-31 22:54:59 -0700

    greedy

---
+++ TopMicrosoftQuestions/_1653_MinimumDeletionstoMakeStringBalanced.js
@@ -0,0 +1,8 @@
+//1653. Minimum Deletions to Make String Balanced
+
+/**
+문자열 s가 'a'와 'b'로만 구성되어 있다고 할 때, 문자열을 **"balanced"**하게 만들고 싶다.
+    "balanced"란, 모든 a는 모든 b보다 앞에만 존재해야 한다는 뜻 
+    예를 들어, "aabbb"는 balanced하지만, "ba" 또는 "abab"은 아님!
+문자열을 balanced하게 만들기 위해 삭제해야 하는 최소 문자 수를 구하는 문제
+ */
