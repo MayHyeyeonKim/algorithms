8c8b6a18f918875c83820700a5dcedbe05e4200d
MayHyeyeonKim <hkim7963@gmail.com>
2025-06-06 22:45:45 -0700

    String, greedy

--- TopMicrosoftQuestions/_2259_RemoveDigitFromNumbertoMaximizeResult.js
+++ TopMicrosoftQuestions/_2259_RemoveDigitFromNumbertoMaximizeResult.js
@@ -1,1 +1,18 @@
 // 2259. Remove Digit From Number to Maximize Result
+
+var removeDigit = function (number, digit) {
+  let max = "";
+
+  for (let i = 0; i < number.length; i++) {
+    if (number[i] === digit) {
+      const candidate = number.slice(0, i) + number.slice(i + 1);
+
+      if (candidate > max) {
+        max = candidate;
+      }
+    }
+  }
+  return max;
+};
+
+console.log(removeDigit("1231", "1"));
