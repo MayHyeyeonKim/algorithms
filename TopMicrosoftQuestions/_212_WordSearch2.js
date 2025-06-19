f15c1fe37bb01765ef96e63a07da65d4387334f6
MayHyeyeonKim <hkim7963@gmail.com>
2025-06-19 12:00:00 -0700

    still working on it

--- TopMicrosoftQuestions/_212_WordSearch2.js
+++ TopMicrosoftQuestions/_212_WordSearch2.js
@@ -1,1 +1,26 @@
 // 212 Word Search II
+
+findWords = function(board, words) {
+    class TrieNode {
+        constructor(){
+            this.children = {};
+            this.word = null;
+        }
+    }
+    // trie 구축
+    const buildTrie = ()
+    // dfs 탐색
+    const dfs = (r,c,node) = {}
+
+    // 보드의 각 셀에서 탐색 시작
+    for(let r = 0; r < rows; r++){
+        for(let c=0; c<cols; c++){
+            dfs(r,c,root)
+        }
+    }
+    return 
+};
+const board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]]
+const words = ["oath","pea","eat","rain"]
+
+console.log(findWords(board,words))
