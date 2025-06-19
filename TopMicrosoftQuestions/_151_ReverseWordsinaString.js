347a3470d038768d12b872e3c3f7e17a42daa428
MayHyeyeonKim <hkim7963@gmail.com>
2025-06-19 12:00:00 -0700

    2025-06-19”

--- TopMicrosoftQuestions/_151_ReverseWordsinaString.js
+++ TopMicrosoftQuestions/_151_ReverseWordsinaString.js
@@ -1,11 +1,12 @@
 // 151. Reverse Words in a String
 
 function reverseWords(s) {
-  let words = s.split(" ");
-  console.log("words 스플릿한 것: ", words);
-  words.reverse();
-  console.log("words 역순으로 뒤집은 것: ", words);
-  return words.join(" ");
+  // let words = s.split(" ");
+  // console.log("words 스플릿한 것: ", words);
+  // words.reverse();
+  // console.log("words 역순으로 뒤집은 것: ", words);
+  // return words.join(" ");
+  return s.trim().split(/\s+/).reverse().join(" ");
 }
 
 let s = "the sky is blue";
@@ -13,5 +14,3 @@ console.log(reverseWords(s));
 
 // Try to think in-place!
 function reverseWords2(s) {}
-
-// return s.trim().split(/\s+/).reverse().join(" ");
