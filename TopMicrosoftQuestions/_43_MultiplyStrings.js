0f9e7b6baa7d26c8eda9ed29dd41e1c30f6a1034
MayHyeyeonKim <hkim7963@gmail.com>
2025-06-23 21:36:57 -0700

    multiply

--- TopMicrosoftQuestions/_43_MultiplyStrings.js
+++ TopMicrosoftQuestions/_43_MultiplyStrings.js
@@ -11,3 +11,25 @@ x   456
  56088
  
  */
+
+function multiply(num1, num2) {
+  const n = num1.length,
+    m = num2.length;
+  const result = Array(n + m).fill(0);
+
+  for (let i = n - 1; i >= 0; i--) {
+    for (let j = m - 1; j >= 0; j--) {
+      const mul = (num1[i] - "0") * (num2[j] - "0");
+      const sum = mul + result[i + j + 1];
+      result[i + j + 1] = sum % 10;
+      result[i + j] += Math.floor(sum / 10);
+    }
+  }
+
+  while (result[0] === 0 && result.length > 1) result.shift();
+  return result.join("");
+}
+
+const num1 = "2";
+const num2 = "3";
+console.log(multiply(num1, num2)); // Output: "6"
