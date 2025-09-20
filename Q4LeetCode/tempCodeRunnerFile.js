2bb8a07312f85df4329e057def3f401a81d9faf1
MayHyeyeonKim <hkim7963@gmail.com>
2025-09-19 22:48:43 -0700

    MinPriorityQueue

---
+++ Q4LeetCode/tempCodeRunnerFile.js
@@ -0,0 +1,25 @@
+import { MinPriorityQueue } from "@datastructures-js/priority-queue";
+
+// 215. Kth Largest Element in an Array
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function (nums, k) {
+  const minHeap = new MinPriorityQueue();
+
+  for (const num of nums) {
+    minHeap.enqueue(num);
+    if (minHeap.size() > k) {
+      minHeap.dequeue();
+    }
+  }
+  const front = minHeap.front();
+  return front ? front.element : null;
+};
+
+// Example usage:
+console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2)); // Output: 5
+console.log(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4)); // Output: 4
