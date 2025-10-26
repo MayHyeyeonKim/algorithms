688ff2d004d322bf55bd4eafcec6cacfc6886e2b
MayHyeyeonKim <hkim7963@gmail.com>
2025-10-25 22:56:11 -0700

    꺄아ㅏ

--- Q4LeetCode/199_BinaryTreeRightSideView.js
+++ Q4LeetCode/199_BinaryTreeRightSideView.js
@@ -1,1 +1,43 @@
 // 199. Binary Tree Right Side View
+
+class TreeNode {
+  constructor(val = 1, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+const rightSideView = (root) => {
+  if (!root) return [];
+
+  const res = [];
+  const queue = [root];
+
+  //   console.log(queue);
+
+  while (queue.length > 0) {
+    const levelSize = queue.length;
+    console.log("Level Size:", levelSize);
+
+    for (let i = 0; i < levelSize; i++) {
+      const cur = queue.shift();
+
+      if (cur.left) queue.push(cur.left);
+      if (cur.right) queue.push(cur.right);
+
+      if (i === levelSize - 1) {
+        res.push(cur.val);
+      }
+    }
+  }
+  return res;
+};
+
+const root = new TreeNode(1);
+root.left = new TreeNode(2);
+root.right = new TreeNode(3);
+root.left.right = new TreeNode(5);
+root.right.right = new TreeNode(4);
+
+console.log(rightSideView(root));
