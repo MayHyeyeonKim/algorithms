4385ea2d2a46fc970365c456fb2ed5e38ff7e0eb
MayHyeyeonKim <hkim7963@gmail.com>
2025-11-23 00:46:17 -0800

    Missing Ranges

--- 2025/Study/MissingRanges.js
+++ 2025/Study/MissingRanges.js
@@ -1,1 +1,31 @@
 // 163. Missing Ranges
+
+function missingRanges(nums, lower, upper) {
+  let result = [];
+  let prev = lower - 1;
+
+  for (let i = 0; i <= nums.length; i++) {
+    let curr;
+
+    if (i < nums.length) {
+      curr = nums[i];
+    } else {
+      curr = upper + 1;
+    }
+
+    if (curr - prev >= 2) {
+      const start = prev + 1;
+      const end = curr - 1;
+
+      if (start === end) {
+        result.push([`${start}`]);
+      } else {
+        result.push([`${start},${end}`]);
+      }
+    }
+    prev = curr;
+  }
+  return result;
+}
+
+console.log(missingRanges([0, 1, 3, 50, 75], 0, 99)); // [["2"],["4->49"],["51->74"],["76->99"]]
