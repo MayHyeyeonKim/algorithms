d8762c9ede114a8bb10f0782f1268222f8304f11
MayHyeyeonKim <hkim7963@gmail.com>
2025-11-25 21:40:00 -0800

    Bidirectional pattern to word validation

--- 2025/Study/290WordPattern.js
+++ 2025/Study/290WordPattern.js
@@ -1,1 +1,29 @@
 // 290. Word Pattern
+
+const wordPattern = (pattern, s) => {
+  const words = s.split(" ");
+
+  const charToWord = new Map();
+  const wordToChar = new Map();
+
+  for (let i = 0; i < pattern.length; i++) {
+    const ch = pattern[i];
+    const word = words[i];
+
+    if (charToWord.has(ch)) {
+      if (charToWord.get(ch) !== word) return false;
+    } else {
+      charToWord.set(ch, word);
+    }
+
+    if (wordToChar.has(word)) {
+      if (wordToChar.get(word) !== ch) return false;
+    } else {
+      wordToChar.set(word, ch);
+    }
+  }
+  return true;
+};
+
+console.log(wordPattern("abba", "dog cat cat dog")); // true
+console.log(wordPattern("abba", "dog cat cat fish"));
