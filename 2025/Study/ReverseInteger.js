1a4271d3f7ec8fed4a854594332e18f0344991ba
MayHyeyeonKim <hkim7963@gmail.com>
2025-12-07 21:52:26 -0800

    reverse integer

--- 2025/Study/ReverseInteger.js
+++ 2025/Study/ReverseInteger.js
@@ -1,1 +1,12 @@
 // 7. Reverse Integer
+
+function reverse(x) {
+  const sign = Math.sign(x);
+  const reversed = parseInt(Math.abs(x).toString().split("").reverse().join(""));
+  const result = sign * reversed;
+
+  return result < -(2 ** 31) || result > 2 ** 31 - 1 ? 0 : result;
+}
+
+console.log(reverse(123)); // 321
+console.log(reverse(-123));
