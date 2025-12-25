94535a650b1aa1e8fb02db85db6b24932e5af043
MayHyeyeonKim <hkim7963@gmail.com>
2025-12-24 21:34:56 -0800

    Find the Smallest Divisor Given a Threshold

---
+++ 2025/December22/1283_FindtheSmallestDivisorGivenaThreshold.js
@@ -0,0 +1,26 @@
+// 1283. Find the Smallest Divisor Given a Threshold
+
+function smallestDivisor(nums, threshold) {
+  let max = Math.max(...nums);
+
+  for (let d = 1; d <= max; d++) {
+    let sum = 0;
+
+    for (let n of nums) {
+      sum += Math.ceil(n / d);
+    }
+
+    if (sum <= threshold) {
+      return d;
+    }
+  }
+}
+
+console.log(smallestDivisor([1, 2, 5, 9], 6)); // 5
+console.log(smallestDivisor([2, 3, 5, 7, 11], 11)); // 3
+console.log(smallestDivisor([19], 5)); // 4
+
+/**
+ * 브루트포스라서 TLE 발생
+ * 이진 탐색으로 해결 해야 함.
+ */
