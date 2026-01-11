613d910467b864e266de61e0991a41722bd2a375
MayHyeyeonKim <hkim7963@gmail.com>
2026-01-10 22:06:14 -0800

    ContainsDuplicate

---
+++ 2026/Blind75/ContainsDuplicate.js
@@ -0,0 +1,16 @@
+// 217. Contains Duplicate
+
+function containsDuplicate(nums) {
+  const set = new Set();
+  for (let num of nums) {
+    if (set.has(num)) {
+      return true;
+    }
+    set.add(num);
+  }
+  return false;
+}
+
+console.log(containsDuplicate([1, 2, 3, 1])); // true
+console.log(containsDuplicate([1, 2, 3, 4])); // false
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])); // trueß
