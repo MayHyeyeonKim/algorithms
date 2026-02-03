a371aa59602a18baac547a349fb45fedbe1a5611
MayHyeyeonKim <hkim7963@gmail.com>
2026-02-02 20:36:59 -0800

    Bitwise AND, XOR

--- 2026/Blind75/SumofTwoIntegers.js
+++ 2026/Blind75/SumofTwoIntegers.js
@@ -1,1 +1,20 @@
 // 371. Sum of Two Integers
+
+function getSum(a, b) {
+  let carry;
+  while ((a & b) !== 0) {
+    carry = (a & b) << 1;
+    a = a ^ b;
+    b = carry;
+  }
+  return a ^ b;
+}
+
+console.log(getSum(1, 2));
+console.log(getSum(-1, 1));
+console.log(getSum(5, 7));
+
+/**
+& : Bitwise AND (AND operator)
+^ : Bitwise XOR (Exclusive OR operator)
+ */
