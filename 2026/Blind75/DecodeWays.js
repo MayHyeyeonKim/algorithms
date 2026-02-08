0fe2f7706a7acfdb993855f5f44983e4c795808c
MayHyeyeonKim <hkim7963@gmail.com>
2026-02-08 10:57:28 -0800

    dp

--- 2026/Blind75/DecodeWays.js
+++ 2026/Blind75/DecodeWays.js
@@ -13,3 +13,27 @@ dp써서
 0 처리만 조심
 
  */
+
+function numDecodings(s) {
+  if (s.length === 0) return 0;
+
+  let prev2 = 1;
+  let prev1 = s[0] === "0" ? 0 : 1;
+
+  for (let i = 2; i <= s.length; i++) {
+    let cur = 0;
+
+    if (s[i - 1] !== "0") {
+      cur += prev1;
+    }
+
+    const two = Number(s.slice(i - 2, i));
+    if (two >= 10 && two <= 26) {
+      cur += prev2;
+    }
+
+    prev2 = prev1;
+    prev1 = cur;
+  }
+  return prev1;
+}
